e2a99363be2cdae029410dcafcca8d9880483355
davidominguezroman <davidominguezroman@gmail.com>
2025-12-07 00:19:33 +0100

    merging

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pag-lab-fis/',
   build: {
     chunkSizeWarningLimit: 7000, // 7000 kB (suppress warning for chunks up to 7 MB)
   },
